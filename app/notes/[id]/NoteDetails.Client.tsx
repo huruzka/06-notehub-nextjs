a21c5556416dd2c912d506033fca7990f74cf0e6
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-10-03 20:34:35 +0300

    Update NoteDetails.Client.tsx

--- app/notes/[id]/NoteDetails.Client.tsx
+++ app/notes/[id]/NoteDetails.Client.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import { fetchSingleNote } from '@/lib/api';
-import css from './NoteDetailsClient.module.css'
+import css from './NoteDetails.client.module.css'
 
 function NoteDetailsClient() {
     const params = useParams();
