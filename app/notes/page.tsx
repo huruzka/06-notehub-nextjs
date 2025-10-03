4a85960489b1c7f038d6acb23f58509cc07d4735
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-10-03 12:11:55 +0300

    hw

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,25 @@
-const Notes = () => {
-  return <div>Notes</div>;
+import { fetchNotes } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import App from './Notes.client';
+
+const topic = '';
+const page = 1;
+
+export default async function Notes () {
+    const QueryClient = new QueryClient();
+
+    await QueryClient.prefetchQuery({
+        queryKey: ['notes', topic, page],
+        queryFn: () => fetchNotes(topic, page),
+    });
+    return (
+        <HydrationBoundary state={dehydrate(QueryClient)}>
+            <App />
+      </HydrationBoundary>
+  );
 };
 
-export default Notes;
---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+
+const modalRoot = document.body;
+
+
+interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+
+const Modal = ({
+    isOpen,
+    onClose,
+    children,
+}: ModalProps): React.ReactPortal | null => {
+    // Обробка клавіші Escape
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Escape") onClose();
+        };
+        if (isOpen) window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [isOpen, onClose]);
+
+    // Блокування прокрутки фону під час відкритої модалки
+    useEffect(() => {
+        if (!isOpen) return;
+        const prevOverflow = document.body.style.overflow;
+        document.body.style.overflow = "hidden"; // заблокувати скролл фону
+        return () => {
+            document.body.style.overflow = prevOverflow;
+        };
+    }, [isOpen]);
+
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) onClose();
+    };
+
+    if (!isOpen) return null;
+
+    return createPortal(
+        <div
+            className={css.backdrop}
+            role="dialog"
+            aria-modal="true"
+            onClick={handleBackdropClick}
+        >
+            <div className={css.modal}>{children}</div>
+        </div>,
+        modalRoot
+    );
+};
+
+export default Modal;
