15dbb9914dfebe255324d5692a222ef123581bd5
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-10-03 20:21:33 +0300

    changed structure

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import NotesClient from './Notes.client';
+import NotesClient from './NotesDetails.client';
 
 const topic = '';
 const page = 1;
